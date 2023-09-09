f53108d6c6f07ec93ed16087db00139619a868a8
Om Kokate <om.kokate2902@gmail.com>
2023-09-09 23:01:13 +0530

    reddit working fine

---
+++ main.py
@@ -0,0 +1,4 @@
+import reddit
+
+if __name__ == "__main__":
+    post_info=reddit.get_top_hot_posts(limit=1)
--- reddit.py
+++ reddit.py
@@ -1,5 +1,6 @@
 import toml
 import praw
+import re
 
 def read_config(filename):
     try:
@@ -10,54 +11,65 @@ def read_config(filename):
         print("Error reading the config file:", e)
         return None
 
-def get_post_and_comments(subreddit_name, post_title, config, comment_limit):
-    try:
-        reddit = praw.Reddit(
-            client_id=config['reddit']['client_id'],
-            client_secret=config['reddit']['client_secret'],
-            user_agent=config['reddit']['user_agent'],
-            username=config['reddit']['username'],
-            password=config['reddit']['password']
-        )
+def beautify_comment(comment_text):
+    # Remove links using regular expression
+    cleaned_comment = re.sub(r'http\S+', '', comment_text)
+    return cleaned_comment
 
-        subreddit = reddit.subreddit(subreddit_name)
-        post = subreddit.search(post_title, limit=1)
-        
-        for submission in post:
-            if len(submission.comments) >= 5:
-                print(f"Post Title: {submission.title}")
-                print(f"Post URL: {submission.url}\n")
+def get_top_hot_posts(limit=3):
+    config = read_config("config.toml")
+    if config:
+        try:
+            reddit = praw.Reddit(
+                client_id=config['reddit']['client_id'],
+                client_secret=config['reddit']['client_secret'],
+                user_agent=config['reddit']['user_agent'],
+                username=config['reddit']['username'],
+                password=config['reddit']['password']
+            )
 
-                comments = submission.comments[:10]
-                for i, comment in enumerate(comments, start=1):
-                    if not comment.stickied and not comment.body == '[removed]' and not comment.body == '[deleted]':
-                        print(f"Comment {i}: {comment.body}")
-            else:
-                print("Fetching another post with minimum 5 comments...")
-                get_another_post(subreddit, post_title, comment_limit)
+            subreddit_name = config['reddit']['subreddit_name']
+            comment_limit = config['reddit']['comment_limit']
+            exclude_nsfw = config['reddit']['exclude_nsfw']
 
-    except praw.exceptions.PRAWException as e:
-        print("PRAW Error:", e)
-    except Exception as e:
-        print("Error:", e)
+            subreddit = reddit.subreddit(subreddit_name)
+            hot_posts = subreddit.hot(limit=None)  # Fetch all hot posts
 
-def get_another_post(subreddit, post_title, comment_limit):
-    for submission in subreddit.search(post_title, limit=None):
-        if len(submission.comments) >= 5:
-            print(f"Post Title: {submission.title}")
-            print(f"Post URL: {submission.url}\n")
+            top_hot_posts = []
+            for post in hot_posts:
+                if not post.over_18 and (not exclude_nsfw or (exclude_nsfw and not post.over_18)):
+                    post_info = {
+                        "title": post.title,
+                        "url": post.url,
+                        "comments": []
+                    }
 
-            comments = submission.comments[:comment_limit]
-            for i, comment in enumerate(comments, start=1):
-                if not comment.stickied and not comment.body == '[removed]' and not comment.body == '[deleted]':
-                    print(f"Comment {i}: {comment.body}")
-            break
+                    comments = post.comments.list()[:comment_limit]
+                    for i, comment in enumerate(comments, start=1):
+                        if not comment.stickied and not comment.body == '[removed]' and not comment.body == '[deleted]':
+                            cleaned_comment = beautify_comment(comment.body)
+                            if cleaned_comment:
+                                post_info["comments"].append(cleaned_comment)
+
+                    top_hot_posts.append(post_info)
+                    if len(top_hot_posts) >= limit:
+                        break  # Stop when you have collected enough posts
+
+            for i, post in enumerate(top_hot_posts, start=1):
+                print(f"Post {i}:")
+                print(f"Title: {post['title']}")
+                print(f"URL: {post['url']}")
+                print("Comments:")
+                for j, comment in enumerate(post['comments'], start=1):
+                    print(f"Comment {j}: {comment}")
+                print("\n")
+            
+            return post_info
+
+        except praw.exceptions.PRAWException as e:
+            print("PRAW Error:", e)
+        except Exception as e:
+            print("Error:", e)
 
 if __name__ == "__main__":
-    config_filename = "config.toml"
-    config = read_config(config_filename)
-    if config:
-        subreddit_name = "askreddit"  # Replace with the subreddit you want to search
-        post_title = "blockchain"  # Replace with the title you want to search
-        comment_limit = 50  # Total number of comments to fetch
-        get_post_and_comments(subreddit_name, post_title, config, comment_limit)
+    get_top_hot_posts(limit)
