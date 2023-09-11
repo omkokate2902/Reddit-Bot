30b199e2873b750c5ca7618b8eef879f8499c6d6
Om Kokate <om.kokate2902@gmail.com>
2023-09-11 19:09:18 +0530

    added comments ss

--- screenshot.py
+++ screenshot.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from PIL import Image
 import os
+import time
 
 # Reddit API credentials
 reddit = praw.Reddit(
@@ -12,7 +13,7 @@ reddit = praw.Reddit(
 )
 
 # Get the post details
-post = reddit.submission(id='16fie14')
+post = reddit.submission(id='16fbfz5')
 
 # Configure Chrome options (optional)
 chrome_options = webdriver.ChromeOptions()
@@ -34,7 +35,7 @@ screenshot = Image.open('assets/screenshots/reddit_post_screenshot.png')
 # You'll need to adjust these coordinates based on the specific layout of the Reddit post page
 left = 0  # Adjust this value as needed
 top = 150   # Adjust this value as needed
-right = 850  # Adjust this value as needed
+right = 900  # Adjust this value as needed
 bottom = 500  # Adjust this value as needed
 
 # Crop the screenshot
@@ -46,6 +47,23 @@ cropped_image.save('assets/screenshots/reddit_question_username.png')
 # Delete the original screenshot file if it exists
 if os.path.exists('assets/screenshots/reddit_post_screenshot.png'):
     os.remove('assets/screenshots/reddit_post_screenshot.png')
+
+comment_urls = [
+    "https://www.reddit.com/r/AskReddit/comments/16fbfz5/comment/k00qpz9",
+    "https://www.reddit.com/r/AskReddit/comments/16fbfz5/comment/k016kud",
+    "https://www.reddit.com/r/AskReddit/comments/16fbfz5/comment/k01b3lq",
+    "https://www.reddit.com/r/AskReddit/comments/16fbfz5/comment/k0155fd",
+]
+
+# Loop through the comment URLs and capture screenshots
+for i, comment_url in enumerate(comment_urls, start=1):
+    # Navigate to the comment URL
+    driver.get(comment_url)
+    time.sleep(5)
+
+    # Capture a screenshot of the comment page
+    screenshot_filename = f'assets/screenshots/comment_{i}.png'
+    driver.save_screenshot(screenshot_filename)
 
 # Close the Chrome driver
 driver.quit()
